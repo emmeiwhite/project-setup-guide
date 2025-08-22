b0297b000254ef351166730f57c19d369b3c39c5
emmeiwhite <emmeiwhite@gmail.com>
2025-08-22 16:30:52 +0530

    setup node.js project

--- src/server.js
+++ src/server.js
@@ -3,3 +3,5 @@ console.log(process.version)
 /**
  * 💡 process.version is a built-in property in Node.js that gives you the active Node version (e.g., v24.4.0).
  */
+
+console.log('Welcome to the Full Stack Development Course!')
