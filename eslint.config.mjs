2ccc81aa59636167f5388a5df3123b706775d692
emmeiwhite <emmeiwhite@gmail.com>
2025-09-01 23:30:17 +0530

    Linting with Type Information

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,8 +5,14 @@ import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
     eslint.configs.recommended,
-    tseslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
     {
         ignores: ['dist', 'node_modules'],
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
     },
 )
