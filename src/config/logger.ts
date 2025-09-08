c114032f3930c8544d710527d386ee8736ef7b89
emmeiwhite <emmeiwhite@gmail.com>
2025-09-08 11:12:53 +0530

    Logging into File

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -6,6 +6,15 @@ const logger = winston.createLogger({
         serviceName: 'auth-service',
     },
     transports: [
+        new winston.transports.File({
+            level: 'info',
+            dirname: 'logs',
+            filename: 'app.log',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+        }),
         new winston.transports.Console({
             level: 'info',
             format: winston.format.combine(
